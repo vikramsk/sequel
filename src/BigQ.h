17b7fd4d02676d53c5fc2a2ee4f7af4dd48e0dc7
Vikram Sreekumar <vikram1791@gmail.com>
2018-02-18 12:26:18 -0500

    sorted: added structures for storing run info.

--- src/BigQ.h
+++ src/BigQ.h
@@ -15,6 +15,13 @@ class BigQ {
 
    private:
     pthread_t worker;
+
+    File runs;
+
+    // runHeads records the page offsets for
+    // each run in the file.
+    vector<off_t> runHeads;
+
     static void *sortRecords(void *voidArgs);
     void mergeRunsAndWrite(Pipe &out, OrderMaker &sortOrder);
     void createRuns(Pipe &in, OrderMaker &sortOrder, int runlen);
