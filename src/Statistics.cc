d51039f5acf344cc4b6dc1c4f666d6465662bbad
Shikha Mehta <shikha.mehta@ufl.edu>
2018-04-07 13:17:34 -0400

    Statistics: Implement CopyRel and deep copy constructor

--- src/Statistics.cc
+++ src/Statistics.cc
@@ -9,7 +9,16 @@ string getString(char *val) {
 }
 
 Statistics::Statistics() {}
-Statistics::Statistics(Statistics &copyMe) {}
+Statistics::Statistics(Statistics &copyMe) {
+    for (auto rel : copyMe.relationStats)
+    {
+        RelationStats rs = *rel.second;
+        relationStats[rel.first] = new RelationStats();
+        relationStats[rel.first]->relations = rs.relations;
+        relationStats[rel.first]->numTuples = rs.numTuples;
+        relationStats[rel.first]->attrDistinctsMap = rs.attrDistinctsMap;
+    }
+}
 Statistics::~Statistics() {}
 
 void Statistics::AddRel(char *relName, int numTuples) {
@@ -20,6 +29,7 @@ void Statistics::AddRel(char *relName, int numTuples) {
         relationStats[rel]->numTuples = numTuples;
     } else {
         relationStats[rel]->numTuples = numTuples;
+        // TODO: Handle case when rel is a set of more than one relations
     }
 }
 
@@ -36,7 +46,18 @@ void Statistics::AddAtt(char *relName, char *attName, int numDistincts) {
     relationStats[rel]->attrDistinctsMap[att] = numDistincts;
 }
 
-void Statistics::CopyRel(char *oldName, char *newName) {}
+void Statistics::CopyRel(char *oldName, char *newName) {
+    string oldRel = getString(oldName);
+    string newRel = getString(newName);
+    if (relationStats.find(oldRel) == relationStats.end()) {
+        cerr << "relation to be copied doesn't exist: " << oldRel << endl;
+        exit(1);
+    }
+    relationStats[newRel] = new RelationStats();
+    relationStats[newRel]->relations = relationStats[oldRel]->relations;
+    relationStats[newRel]->numTuples = relationStats[oldRel]->numTuples;
+    relationStats[newRel]->attrDistinctsMap = relationStats[oldRel]->attrDistinctsMap;
+}
 
 void Statistics::Read(char *fromWhere) {}
 void Statistics::Write(char *fromWhere) {}
