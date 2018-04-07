2b5a288adb5ce64ee226087f1b51c168b63c2104
Vikram Sreekumar <vikram1791@gmail.com>
2018-04-06 20:09:38 -0400

    stats: addrel, addattr implemented

--- src/Statistics.cc
+++ src/Statistics.cc
@@ -1,13 +1,41 @@
 #include "Statistics.h"
+#include <iostream>
 #include <string>
 #include <unordered_map>
+
+string getString(char *val) {
+    std::string str(val);
+    return str;
+}
 
 Statistics::Statistics() {}
 Statistics::Statistics(Statistics &copyMe) {}
 Statistics::~Statistics() {}
 
-void Statistics::AddRel(char *relName, int numTuples) {}
-void Statistics::AddAtt(char *relName, char *attName, int numDistincts) {}
+void Statistics::AddRel(char *relName, int numTuples) {
+    string rel = getString(relName);
+    if (relationStats.find(rel) == relationStats.end()) {
+        relationStats[rel] = new RelationStats();
+        relationStats[rel]->relations.insert(rel);
+        relationStats[rel]->numTuples = numTuples;
+    } else {
+        relationStats[rel]->numTuples = numTuples;
+    }
+}
+
+void Statistics::AddAtt(char *relName, char *attName, int numDistincts) {
+    string rel = getString(relName);
+    string att = getString(attName);
+    if (relationStats.find(rel) == relationStats.end()) {
+        cerr << "relation doesn't exist for given attribute: " << rel << endl;
+        exit(1);
+    }
+    if (numDistincts == -1) {
+        numDistincts = relationStats[rel]->numTuples;
+    }
+    relationStats[rel]->attrDistinctsMap[att] = numDistincts;
+}
+
 void Statistics::CopyRel(char *oldName, char *newName) {}
 
 void Statistics::Read(char *fromWhere) {}
