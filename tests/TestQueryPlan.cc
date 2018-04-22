46ed14a22b2ee6e5661df6fe7eff9034612531ba
Shikha Mehta <shikha.mehta@ufl.edu>
2018-04-21 23:02:14 -0400

    Update query number in test case

--- tests/TestQueryPlan.cc
+++ tests/TestQueryPlan.cc
@@ -4,7 +4,7 @@
 #include "QueryPlanner.h"
 #include "ParseTree.h"
 
-TEST(QueryPlanTest, Query1) {
+TEST(QueryPlanTest, Query10) {
     char *query = "SELECT SUM (l.l_extendedprice * l.l_discount) FROM lineitem AS l WHERE (l.l_discount<0.07) AND (l.l_quantity < 24)";
     init_sql_parser(query);
     yysqlparse();
