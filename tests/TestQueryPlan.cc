07220ad03306581a8fe16a36820b9c60fbb485dc
Shikha Mehta <shikha.mehta@ufl.edu>
2018-04-22 01:31:05 -0400

    Add query 6 to tests suite

--- tests/TestQueryPlan.cc
+++ tests/TestQueryPlan.cc
@@ -6,6 +6,20 @@
 
 TEST(QueryPlanTest, Query3) {
     char *query = "SELECT l.l_orderkey, l.l_partkey, l.l_suppkey FROM lineitem AS l WHERE (l.l_returnflag = 'R') AND (l.l_discount < 0.04 OR l.l_shipmode = 'MAIL')";
+    cout << endl << query << endl;
+    init_sql_parser(query);
+    yysqlparse();
+    close_sql_parser();
+    QueryTokens qt(finalFunction, tables, boolean, groupingAtts,
+                    attsToSelect, distinctAtts, distinctFunc);
+    QueryPlanner qp(qt);
+    qp.Create();
+    qp.Print();
+}
+
+TEST(QueryPlanTest, Query6) {
+    char *query = "SELECT l.l_orderkey FROM lineitem AS l WHERE (l.l_quantity > 30)";
+    cout << endl << query << endl;
     init_sql_parser(query);
     yysqlparse();
     close_sql_parser();
@@ -18,6 +32,7 @@ TEST(QueryPlanTest, Query3) {
 
 TEST(QueryPlanTest, Query10) {
     char *query = "SELECT SUM (l.l_extendedprice * l.l_discount) FROM lineitem AS l WHERE (l.l_discount<0.07) AND (l.l_quantity < 24)";
+    cout << endl << query << endl;
     init_sql_parser(query);
     yysqlparse();
     close_sql_parser();
